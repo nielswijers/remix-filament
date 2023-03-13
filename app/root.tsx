395f135f490de3936c9714a2dba336458a16d9f7
Niels Wijers <nielswijers@gmail.com>
2023-03-13 13:55:18 +0100

    add filament

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-import type { MetaFunction } from "@remix-run/node";
+import type { LinksFunction, MetaFunction } from '@remix-run/node';
 import {
   Links,
   LiveReload,
@@ -6,12 +6,27 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+} from '@remix-run/react';
+import fonts from '@filament-theme/fonts';
+import { cssBundleHref } from '@remix-run/css-bundle';
+import { Button } from '@filament/react';
+
+import { base } from '@filament-theme/base';
+import { ddls, DLSProfessional } from '@filament-theme/ddls';
+
+// console.log(cssBundleHref);
+
+export const links: LinksFunction = () => {
+  return [
+    { rel: 'stylesheet', href: fonts },
+    ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
+  ];
+};
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+  charset: 'utf-8',
+  title: 'New Remix App',
+  viewport: 'width=device-width,initial-scale=1',
 });
 
 export default function App() {
@@ -21,7 +36,15 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body
+        className={`${ddls} ${DLSProfessional.groupBlueLight} ${DLSProfessional.orangeDarkAccent} ${DLSProfessional.main} ${base}`}
+      >
+        <Button>yeah</Button>
+        <Button variant="primary">yeah</Button>
+        <Button variant="secondary">yeah</Button>
+        <Button variant="quiet">yeah</Button>
+        <Button variant="quiet">yeah</Button>
+        <Button variant="quietEmphasis">yeadsddash</Button>
         <Outlet />
         <ScrollRestoration />
         <Scripts />
--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,19 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
+  serverDependenciesToBundle: [
+    /^@filament/,
+    /^@filament-theme/,
+    /^@filament-react/,
+  ],
   future: {
     v2_routeConvention: true,
+    unstable_vanillaExtract: true,
+    unstable_cssSideEffectImports: true,
   },
-  ignoredRouteFiles: ["**/.*"],
+  ignoredRouteFiles: ['**/.*'],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
   // publicPath: "/build/",
+  cacheDirectory: './node_modules/.cache/remix',
 };
